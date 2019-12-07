af6467968ce76030664d3871e39cc0254f850a90
artem <etomojapo4ta@gmail.com>
2019-12-07 15:07:26 +0200

    car without thread

--- Program.cs
+++ Program.cs
@@ -1,53 +1,37 @@
-﻿using System;
-using System.Threading;
+﻿    using System;
 
 namespace myconsole
 {
-
+  
     class Program
     {
-        static bool isDone;
+        
         static void Main(string[] args)
         {
-            var car = new Car("©", 500);
+            var car = new Car("©", 10);
             car.MoveForward();
 
+            var isDone = true;
 
-            Thread drawThread;
-            ControllSetup(car, out drawThread);
-
-            while (isDone)
+            while(isDone)
             {
-                Thread.Sleep(100);
-                car.MoveForward();
-            }
-            Console.WriteLine("full stop");
-        }
-
-        private static void ControllSetup(Car car, out Thread drawThread)
-        {
-            isDone = true;
-            drawThread = new Thread(() =>
-            {
-                while (isDone)
+                var key = Console.ReadKey();
+                switch (key.Key)
                 {
-
-                    var key = Console.ReadKey();
-                    switch (key.Key)
-                    {
-                        case ConsoleKey.LeftArrow:
-                            car.TurnLeft();
-                            break;
-                        case ConsoleKey.RightArrow:
-                            car.TurnRight();
-                            break;
-                        default:
-                            isDone = false;
-                            break;
-                    }
+                    case ConsoleKey.LeftArrow:
+                        car.TurnLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        car.TurnRight();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        car.MoveForward();
+                        break;
+                    default:
+                        isDone = false;
+                        break;
                 }
-            });
-            drawThread.Start();
+            }
         }
     }
 }
